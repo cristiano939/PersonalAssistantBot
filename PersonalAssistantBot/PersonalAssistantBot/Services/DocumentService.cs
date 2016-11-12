88574952720062264ee7bb2f9770034171e9a6da
cristiano939 <cristiano939@msn.com>
2016-11-12 14:42:36 -0200

    created document service

---
+++ PersonalAssistantBot/PersonalAssistantBot/Services/DocumentService.cs
@@ -0,0 +1,44 @@
+﻿using Lime.Messaging.Contents;
+using Lime.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAssistantBot.Services
+{
+    public class DocumentService
+    {
+
+        public MediaLink CreateMediaLink(string title, string text, Uri url, string mediatype)
+        {
+            var type = mediatype.Split('/')[0];
+            var subtype = mediatype.Split('/')[1];
+            MediaLink mediaLink = new MediaLink
+            {
+                Title = title,
+                Text = text,
+                PreviewUri = url,
+                Uri = url,
+                Size = 1,
+                Type = new MediaType(type, subtype)
+
+            };
+            return mediaLink;
+        }
+
+        public WebLink CreateWebLink(string title, string text, Uri url, string mediatype)
+        {
+            WebLink webLink = new WebLink
+            {
+                Title = title,
+                Text = text,
+                Uri = url,
+                PreviewUri = url,
+                PreviewType = MediaType.Parse(mediatype)
+            };
+            return webLink;
+        }
+    }
+}
